ce0d873b7b7c0da72661e24eb547203f95a4afb3
LeopoldBriand <lebriand@boccard.fr>
2023-07-12 20:44:06 +0000

    feat: get tl code from file or folder

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
+use std::{env, fs};
+use std::fs::metadata;
 mod lexer;
 
 fn main() {
-    let mut lex = lexer::LexicalParser::new("
-    var toto = 12 + 5;
-    if (toto > 6){
-        print('more than 6');
-    } else {
-        print('less than 6');
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => {
+            let path = args[1].clone();
+            let content: String;
+            match metadata(&path) {
+                Ok(p) => {
+                    if p.is_dir() {
+                        content = fs::read_to_string(format!("{path}/index.tl"))
+                            .expect("Compiler is not able to read the file");
+                    } else {
+                        content = fs::read_to_string(path)
+                            .expect("Compiler is not able to read the file");
+                    }
+                },
+                Err(_) => panic!("Error while accessing the file")
+            }
+            let mut lex = lexer::LexicalParser::new(content);
+            let result = lex.parse();
+            println!("{:?}", result);
+        },
+        _ => panic!("Compiler need only one given argument")
     }
-    ".to_string());
-    let result = lex.parse();
-    println!("{:?}", result);
 }
