70c52adfe847c881c98daa9d457ba572ca1176f0
LeopoldBriand <leopold.briand@laposte.net>
2023-08-11 12:18:04 +0200

    refactor: add clap for cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
-use std::fs::metadata;
-use std::{env, fs};
+use std::fs;
 mod grammar;
 mod lexer;
 mod parser;
 
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    path: Option<String>,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        2 => {
-            let path = args[1].clone();
-            let content: String;
-            match metadata(&path) {
-                Ok(p) => {
-                    if p.is_dir() {
-                        content = fs::read_to_string(format!("{path}/index.toy"))
-                            .expect("Compiler is not able to read the file");
-                    } else {
-                        content = fs::read_to_string(path)
-                            .expect("Compiler is not able to read the file");
-                    }
-                }
-                Err(_) => panic!("Error while accessing the file"),
-            }
-            let mut lex = lexer::LexicalParser::new(content);
-            match lex.parse() {
-                Ok(lexicon) => {
-                    let mut parser = parser::SyntaxAnalizer::new(lexicon);
-                    parser.parse();
-                    let result = parser.ast;
-                    println!("{:?}", result);
-                }
-                Err(error) => panic!("Lexical error: {}", error.as_ref().to_string()),
+    let cli = Cli::parse();
+    let path = cli.path.unwrap();
+    let content: String;
+    match fs::metadata(&path) {
+        Ok(p) => {
+            if p.is_dir() {
+                content = fs::read_to_string(format!("{path}/index.toy"))
+                    .expect("Compiler is not able to read the file");
+            } else {
+                content = fs::read_to_string(path)
+                    .expect("Compiler is not able to read the file");
             }
         }
-        _ => panic!("Compiler need only one given argument"),
+        Err(_) => panic!("Error while accessing the file"),
     }
+    let mut lex = lexer::LexicalParser::new(content);
+    match lex.parse() {
+        Ok(lexicon) => {
+            let mut parser = parser::SyntaxAnalizer::new(lexicon);
+            parser.parse();
+            let result = parser.ast;
+            println!("{:?}", result);
+        }
+        Err(error) => panic!("Lexical error: {}", error.as_ref().to_string()),
+    }
+    
 }
